c474368a6f9e6414cfa0747e9301934f373db6f4
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2019-03-11 14:30:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Alex Erlandsson
+
+This is the repository for https://alexerlandsson.com.
+
+## Before you start
+
+### Install dependencies (npm)
+
+```shell
+$ npm install
+```
+
+## Build
+
+This repsoitory uses gulp to build the project. Make the changes in `/src`. To build the project, run gulp from the project root. After the project has been built, it will be put in the folder `dist`. This folder contains all assets used in production.
+
+```shell
+$ gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-clean-css');
+var replace = require('gulp-replace');
+var fs = require('fs');
+
+var paths = {
+  sass: {
+    src: './src/style/default.scss',
+    dest: './dist/alexerlandsson/style',
+    watch: './src/style/**/*.scss'
+  },
+  html: {
+    src: './src/index.html',
+    dest: './dist'
+  },
+  images: {
+    src: './src/images/**/*',
+    dest: './dist/alexerlandsson/images'
+  }
+};
+
+// CSS
+
+gulp.task('sass', function() {
+  return gulp
+    .src(paths.sass.src)
+    .pipe(sass())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest(paths.sass.dest));
+});
+
+gulp.task('watch:sass', function() {
+  gulp.watch(paths.sass.watch, gulp.series('sass'));
+});
+
+gulp.task('watch', gulp.series('sass',
+  gulp.parallel('watch:sass')
+));
+
+// HTML
+
+gulp.task('html', function() {
+  return gulp
+    .src(paths.html.src)
+    .pipe(replace(/<link href="([^\.]+\.css)"[^>]*>/g, function(s) {
+      var style = fs.readFileSync(paths.sass.dest + '/default.min.css', 'utf8');
+      return '<style>\n\t\t\t' + style + '\n\t\t</style>';
+    }))
+    .pipe(gulp.dest(paths.html.dest));
+});
+
+// Images
+
+gulp.task('images', function() {
+  return gulp
+    .src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest));
+});
+
+// Default
+
+gulp.task('default', gulp.series('sass', 'html', 'images'));
